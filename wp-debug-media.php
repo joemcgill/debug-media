3c954505d7655c800c1d167b10ecf22f207bcc04
Joe McGill <joemcgill@gmail.com>
2016-05-23 14:24:56 -0500

    Initial plugin scaffolding

---
+++ wp-debug-media.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WP Media Debugger
+ * Version: 0.1-alpha
+ * Description: Output helpful debugging information related to your media settings.
+ * Author: Joe McGill
+ * Author URI: joemcgill.net
+ * Plugin URI: https://github.com/joemcgill/wp-debug-media
+ * Text Domain: wp-debug-media
+ * Domain Path: /languages
+ * @package WP_Debug_Media
+ */

9e36d0f277f9ca5fbffb5ec2b9c91f30e3afd5dd
Joe McGill <joemcgill@gmail.com>
2016-05-23 15:26:28 -0500

    Update the readme and change plugin name

--- wp-debug-media.php
+++ wp-debug-media.php
@@ -1,12 +1,19 @@
 <?php
 /**
- * Plugin Name: WP Media Debugger
- * Version: 0.1-alpha
+ * Plugin Name: Debug Media
+ * Version: 0.1.0
  * Description: Output helpful debugging information related to your media settings.
  * Author: Joe McGill
  * Author URI: joemcgill.net
- * Plugin URI: https://github.com/joemcgill/wp-debug-media
- * Text Domain: wp-debug-media
+ * Plugin URI: https://github.com/joemcgill/debug-media
+ * Text Domain: debug-media
  * Domain Path: /languages
- * @package WP_Debug_Media
+ * @package Debug_Media
  */
+
+add_filter( 'debug_bar_panels', 'debug_bar_media_panel' );
+function debug_bar_media_panel( $panels ) {
+	require_once 'class-debug-bar-media.php';
+	$panels[] = new Debug_Bar_Media();
+	return $panels;
+}

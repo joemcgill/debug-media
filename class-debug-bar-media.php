6202438f94c60e793c71e36f0eb813af4f8df3c9
Joe McGill <joemcgill@gmail.com>
2016-06-09 23:07:16 -0500

    Add Imagick Resource limit info.
    If the current editor is `WP_Image_Editor_Imagick`, this will
    show all of the Imagick resource limits on the server.
    
    See: http://php.net/manual/en/imagick.constants.php#imagick.constants.resourcetypes

--- class-debug-bar-media.php
+++ class-debug-bar-media.php
@@ -18,6 +18,15 @@ class Debug_Bar_Media extends Debug_Bar_Panel {
 	/**
 	 * Imagick version info.
 	 *
+	 * @since  0.2.0
+	 * @access private
+	 * @var    Imagick|string
+	 */
+	private $imagick = '';
+
+	/**
+	 * Imagick version info.
+	 *
 	 * @since  0.1.0
 	 * @access private
 	 * @var    array|string
@@ -97,6 +106,24 @@ class Debug_Bar_Media extends Debug_Bar_Panel {
 			</tr>
 		</tbody>
 	</table>
+
+	<?php if ( 'WP_Image_Editor_Imagick' === $this->editor ) : ?>
+	<h2>Imagick Resource Limits</h2>
+	<table class="form-table">
+		<tbody>
+		<?php
+		$limits = $this->get_imagick_resource_limits();
+
+		foreach ( $limits as $limit => $value ) { ?>
+			<tr>
+				<th scope="row">Imagick <?php echo esc_html( $limit ) ?>  limit</th>
+				<td><?php echo esc_html( $value ); ?></td>
+			</tr>
+		<?php } ?>
+		</tbody>
+	</table>
+	<?php endif; ?>
+
 	<?php
 	}
 
@@ -130,8 +157,9 @@ class Debug_Bar_Media extends Debug_Bar_Panel {
 		// Return early if we already know the Imagick version.
 		if ( ! $this->imagick_version ) {
 			if ( class_exists( 'Imagick' ) ) {
-				$imagick = new Imagick();
-				$this->imagick_version = $imagick->getVersion();
+				// Save the Imagick instance for later use.
+				$this->imagick = new Imagick();
+				$this->imagick_version = $this->imagick->getVersion();
 			} else {
 				$this->imagick_version = 'Imagick not available';
 			}
@@ -160,5 +188,27 @@ class Debug_Bar_Media extends Debug_Bar_Panel {
 		}
 
 		return $this->gd_version;
+	}
+
+	/**
+	 * Return the Imagick resource limits
+	 *
+	 * @since  0.2.0
+	 * @access private
+	 * @return array
+	 */
+	private function get_imagick_resource_limits() {
+		static $limits = array();
+
+		if ( empty( $limits ) && $this->imagick instanceof Imagick ) {
+				$limits['area']   = ( defined( 'imagick::RESOURCETYPE_AREA' ) )   ? size_format( $this->imagick->getResourceLimit( imagick::RESOURCETYPE_AREA ) ) : 'Not Available';
+				$limits['disk']   = ( defined( 'imagick::RESOURCETYPE_DISK' ) )   ? $this->imagick->getResourceLimit( imagick::RESOURCETYPE_DISK ) : 'Not Available';
+				$limits['file']   = ( defined( 'imagick::RESOURCETYPE_FILE' ) )   ? $this->imagick->getResourceLimit( imagick::RESOURCETYPE_FILE ) : 'Not Available';
+				$limits['map']    = ( defined( 'imagick::RESOURCETYPE_MAP' ) )    ? size_format( $this->imagick->getResourceLimit( imagick::RESOURCETYPE_MAP ) ) : 'Not Available';
+				$limits['memory'] = ( defined( 'imagick::RESOURCETYPE_MEMORY' ) ) ? size_format( $this->imagick->getResourceLimit( imagick::RESOURCETYPE_MEMORY ) ) : 'Not Available';
+				$limits['thread'] = ( defined( 'imagick::RESOURCETYPE_THREAD' ) ) ? $this->imagick->getResourceLimit( imagick::RESOURCETYPE_THREAD ) : 'Not Available';
+		}
+
+		return $limits;
 	}
 }
